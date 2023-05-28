b97c12ef5b0e8af8035e1508f446468e5d5fd1bc
JosephBurger <85204704+JosephBurger@users.noreply.github.com>
2023-05-28 15:38:42 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+## 1 - Within a Github action that runs whenever code is pushed because it allows the devloper to automate the process, as well as providing the developer with the ability to test while developing.
+
+## 2 - No
+
+## 3 - I would use end-to-end testing here because the messaging feature likely involves multiple componenets and thus unit testing might not be very efficient.
+
+## 4 - Unit testing because checking the length of a message can be done with a very simple length check test function.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,74 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
+//isPhoneNUumber Tests
+test('tests if phone number 760-315-9933 is valid', () => {
+    expect(functions.isPhoneNumber('760-315-9933')).toBe(true);
+});
+test('tests if phone number 760-315-9934 is valid', () => {
+    expect(functions.isPhoneNumber('760-315-9934')).toBe(true);
+});
+test('tests if phone number 760-315-99344 is valid', () => {
+    expect(functions.isPhoneNumber('760-315-99344')).toBe(false);
+});
+test('tests if phone number 760-315-9934 is valid', () => {
+    expect(functions.isPhoneNumber('7A0-315-9934')).toBe(false);
+});
+
+//isEmail Tests
+test('tests if jburgos@ucsd.edu is valid', () => {
+    expect(functions.isEmail('jburgos@ucsd.edu')).toBe(true);
+});
+test('tests if lburgos@gmail.com is valid', () => {
+    expect(functions.isEmail('lburgos@gmail.com ')).toBe(true);
+});
+test('tests if lburgos@gmail.com.com is valid', () => {
+    expect(functions.isEmail('lburgos@gmail.com.com')).toBe(false);
+});
+test('tests if jburgosgmail.com is valid', () => {
+    expect(functions.isEmail('jburgosgmail.com')).toBe(false);
+});
+
+//isStrongPassword Tests
+test('tests if PeterPan is a Strong Password', () => {
+    expect(functions.isEmail('PeterPan')).toBe(true);
+});
+test('tests if Melon_Lover55 is a Strong Password', () => {
+    expect(functions.isEmail('Melon_Lover55')).toBe(true);
+});
+test('tests if 5PeterPan is a Strong Password', () => {
+    expect(functions.isEmail('5PeterPan')).toBe(false);
+});
+test('tests if Peter&(Pan is a Strong Password', () => {
+    expect(functions.isEmail('Peter&(Pan')).toBe(false);
+});
+
+//isData Tests
+test('tests if 11/20/2001 is a valid date', () => {
+    expect(functions.isDate('11/20/2001')).toBe(true);
+});
+test('tests if 11/20/2001 is a valid date', () => {
+    expect(functions.isDate('4/20/1800')).toBe(true);
+});
+test('tests if November 20th,2001 is a valid date', () => {
+    expect(functions.isDate('November 20th,2001')).toBe(false);
+});
+test('tests if 11/20/20011 is a valid date', () => {
+    expect(functions.isDate('11/20/20011')).toBe(false);
+});
+
+
+//isHexColor Tests
+test('test if #008000 is a valid hex color code', () => {
+    expect(functions.isHexColor('#008000')).toBe(true);
+});
+test('test if #FF0000 is a valid hex color code', () => {
+    expect(functions.isHexColor('#FF0000')).toBe(true);
+});
+test('test if #AA33FF11 is a valid hex color code', () => {
+    expect(functions.isHexColor('#AA33FF11')).toBe(false);
+});
+test('test if #Z34FF9 is a valid hex color code', () => {
+    expect(functions.isHexColor('#Z34FF9')).toBe(false);
+});
 // TODO - Part 2
